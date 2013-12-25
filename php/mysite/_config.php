308dcbaccb2f5556abdc3c367f9948ff6aba9570
Lloyd Emelle <lloyd@emelle.me>
2013-12-25 08:44:34 +0000

    updates

--- php/mysite/_config.php
+++ php/mysite/_config.php
@@ -3,10 +3,15 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = '';
-
-require_once('conf/ConfigureFromEnv.php');
+global $databaseConfig;
+$databaseConfig = array(
+        "type" => 'MySQLDatabase',
+        "server" => '127.2.188.130',
+        "username" => $_ENV['OPENSHIFT_MYSQL_DB_HOST'],
+        "password" => 'bc68E-QSE8Fd',
+        "database" => 'parcels',
+        "port" => $_ENV['OPENSHIFT_MYSQL_DB_PORT']
+);
 
 // Set the site locale
 i18n::set_locale('en_US');
