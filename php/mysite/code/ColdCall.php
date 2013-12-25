e28425f91b9fa4f910fe9cac5a7c662a60092d6c
Lloyd Emelle <lloyd@emelle.me>
2013-12-25 11:12:56 +0000

    twilio fix

--- php/mysite/code/ColdCall.php
+++ php/mysite/code/ColdCall.php
@@ -33,12 +33,12 @@ class ColdCall extends Controller {
         printf('Initiating Call...');
         // This task initiates a cold call to the number passed by variable
         $t = new Services_Twilio($_ENV['TWILIO_SID'],$_ENV['TWILIO_TOKEN']);
-        $t = $client->account->calls->create(
+        $tcall = $t->account->calls->create(
         '8325166865', // From a valid Twilio number
         '7137020650', // Call this number
 
         // Read TwiML at this URL when a call connects (hold music)
-        'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient'
+        'http://twimlets.com/voicemail?Email=lloyd%40emelle.me&Message=Leave%20a%20Message&Transcribe=true&'
         );
         
         list($usec, $sec) = explode(' ', microtime());
