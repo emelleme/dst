b3a20621a6cee8faca4bcdcbc1458d15290e7670
Lloyd Emelle <lloyd@emelle.me>
2013-12-27 05:29:31 +0000

    updates

--- php/mysite/code/ParcelPage.php
+++ php/mysite/code/ParcelPage.php
@@ -9,7 +9,7 @@ class ParcelPage_Controller extends Page_Controller {
     );
     public function SoundForm() {
         $fields = new FieldList(
-            $field = new UploadField('SoundClip', 'Upload SoundClip (minimum 15 sec; must be mp3)'),
+            $field1 = new UploadField('SoundClip', 'Upload SoundClip (minimum 15 sec; must be mp3)'),
             $l = new LiteralField('Notice', 'Note, the previous sound will be removed.'),
             $field2 = new UploadField('ParcelsFile', 'Upload CSV of Parcels'),
             $l2 = new LiteralField('ParcelsFormat', 'CSV should contain phone number fields.')
@@ -18,7 +18,8 @@ class ParcelPage_Controller extends Page_Controller {
         $field1->setAllowedExtensions(array('mp3','wav')); // Don't show target filesystem folder on upload field
         $field1->setFolderName('csv'); // Block access to Silverstripe assets library
         $field1->setAllowedExtensions(array('csv')); // Don't show target filesystem folder on upload field
-        $field->relationAutoSetting = false; // Prevents the form thinking the GalleryPage is the underlying object
+        $field1->relationAutoSetting = false; // Prevents the form thinking the GalleryPage is the underlying object
+        $field2->relationAutoSetting = false; // Prevents the form thinking the GalleryPage is the underlying object
         $actions = new FieldList(new FormAction('submit', 'Save'));
         return new Form($this, 'submit', $fields, $actions, null);
     }
